cff40c1bafa43ab0c6d0728ad2eb60b17d9485af
Pedro Machado <pedromchd@outlook.com.br>
2022-11-07 20:57:17 -0300

    tailwindcss install and setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@shrutibalasa/tailwind-grid-auto-fit"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+    require("prettier-plugin-tailwindcss")
+  ],
+}
